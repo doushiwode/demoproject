cd9aa2276329c3fda85f37e69a19ba8301ab30d9
doushiwode <128155045+doushiwode@users.noreply.github.com>
2025-01-01 00:20:25 +0800

    头文件又改了一下

--- header.h
+++ header.h
@@ -1,20 +1,13 @@
-// 防止头文件被重复包含
 #ifndef HEADER_H
 #define HEADER_H
 
-// 如果是 C++ 编译器，使用 extern "C" 来指示使用 C 语言的链接方式
-#ifdef __cplusplus
-extern "C" {
-#endif
+// Include necessary standard libraries
+#include <iostream>
+#include <string>
 
-// 示例函数声明
-// 这个函数在其他地方实现，可以在其他文件中包含这个头文件来调用它
+// Function declarations
 void exampleFunction();
+int add(int a, int b);
+std::string getGreeting(const std::string& name);
 
-// 如果是 C++ 编译器，结束 extern "C" 块
-#ifdef __cplusplus
-}
-#endif
-
-// 结束头文件防重复包含的宏定义
 #endif // HEADER_H
